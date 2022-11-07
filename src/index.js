971798de8fd04832d56922e735e26ff12469b37e
joshdoyle94 <joshdoyle94@gmail.com>
2022-11-06 22:26:53 -0500

    added first run through of components build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const toDos = ['walk Snoopy', 'make breakfast', 'think about code', 'sleep']
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos}/>,
   document.getElementById('root')
-);
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
